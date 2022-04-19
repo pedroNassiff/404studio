619a2d7834547a263da6c31f72fe51f9590020f6
Pedro Nassiff <pedro.nassiff@fractalys.pro>
2022-04-19 22:53:41 +0200

    feature-service-location-ip

---
+++ pages/userdata.js
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react'
+import {getIP} from "../components/services"
+
+const userData = (props) => {
+    const [details, setDetails] = useState(null);
+
+    const getUserDetails = () => {
+        fetch("https://geolocation-db.com/json/d802faa0-10bd-11ec-b2fe-47a0872c6708")
+        .then( response => response.json() )
+        .then( data => setDetails(data) )
+    }
+   
+  return (
+    <div>
+        <button onClick={getUserDetails}>Ver ip</button>
+        <div>
+            {details && 
+                <ul>
+                    <li>
+                        Lugar: {`${details.city}, ${details.country_name}, ${details.country_code},`}
+                    </li>
+                    <li>IP: {details.IPv4}</li>
+                </ul>}
+        </div>
+    </div>
+  )
+}
+
+export default userData;
