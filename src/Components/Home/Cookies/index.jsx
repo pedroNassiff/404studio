690cdb1291f03931fbb1140319fd84dfabdbb550
Pedro Nassiff <pedro.nassiff@fractalys.pro>
2022-08-05 15:21:28 +0200

    bug-cookies

--- src/Components/Home/Cookies/index.jsx
+++ src/Components/Home/Cookies/index.jsx
@@ -3,26 +3,38 @@ import hero from "./styles.module.scss";
 import VolumeOffIcon from "@mui/icons-material/VolumeOff";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 import CookieConsent, { Cookies, getCookieConsentValue } from "react-cookie-consent";
+import { getCookies, getCookie, setCookie, deleteCookie } from 'cookies-next';
 
 import gsap from "gsap";
 
 export default function Cookies404() {
   const [btnAcceptState, setbtnAcceptState] = useState(true)
-  console.log(getCookieConsentValue());
+  const [btnCancel, setBtnCancel] = useState(true)
   const cookieConsentRef = useRef();
   const ref = useRef();
 
-  const aceptFunction = (acceptedByScrolling) => {
-    console.log("getCookieConsentValue", getCookieConsentValue());
-    cookieConsentRef.current.accept();
-    if (acceptedByScrolling & document.cookie) {
-      setbtnAcceptState(false)
-      alert()
-    } else {
-      setbtnAcceptState(false)
+  const getCokkie = () => {
+    let id = getCookie('key')
+    if (id =='somos404'){
+      console.log("key====", id);
+      setbtnAcceptState(true)
     }
   }
 
+  const aceptFunction = (acceptedByScrolling) => {
+    setCookie('key', 'somos404');
+    setbtnAcceptState(false)
+  }
+
+  const cancelFuncion = () => {
+    setBtnCancel(false);
+    setbtnAcceptState(false)
+
+  }
+
+useEffect(() => {
+  getCokkie();
+}, [])
 
 
   // --------------------------------
@@ -49,60 +61,60 @@ export default function Cookies404() {
 
   return (
 
-        <div className={`${hero["containerCookies"]} `} style={{ position: 'fixed', width: '800px', height: '225px'}}>
-          <div className={`${hero["hero_bottom"]} `}>
-            <div
-              className={`${hero["hero_bottom__container"]} aspect_ratio aspect_ratio--16by9`}
-            >
-              <div
-                className={`${hero["hero_bottom__frame"]} ${hero["hero_bottom__frame--one"]}`}
-              ></div>
-              <div
-                className={`${hero["hero_bottom__frame"]} ${hero["hero_bottom__frame--two"]}`}
-              ></div>
-  
-  
-              <div
-                className={`${hero["hero_bottom__frame"]} ${hero["hero_bottom__frame--three"]} aspect_ratio__item object_fit_cover div-video`}
-              >
-                <div>
-                  <div><h1  style={{ textAlign: 'center', color: 'yellow', fontWeight: '700', fontFamily: 'Helvetica', fontSize:'35px', }}>WELCOME TO OUR WORLD</h1></div>
-                  <div><h2 style={{ textAlign: 'center',  fontWeight: '700',fontFamily: 'Helvetica', fontSize: '22px', marginTop: '-15px'}}>AVISO DE COOKIES</h2></div>
-                </div>
-                <span style={{ display: 'flex', justifyContent: ' center' }}>
-                  <button 
-                  style={{ border: "none", color: 'blue', fontWeight: '700', width: "200px", height: "50PX", marginLeft: "0px",fontSize: '24px', cursor: 'pointer'}}
-                  onClick={aceptFunction}>OK</button>
-                  <button 
-                  style={{ border: "none", color: 'blue', fontWeight: '700', width: "200px", height: "50PX", marginLeft: "100px", fontSize: '24px', cursor: 'pointer'}}
-                  className="buttonModal">CANCEL</button>
-                </span>
-                <div style={{ }}>
-                  <CookieConsent
-                    ref={cookieConsentRef}
-                    onAccept={(acceptedByScrolling) => {
-                      if (acceptedByScrolling) {
-                        // triggered if user scrolls past threshold
-                      } else {
-                      }
-                    }}
-                    cookieName="CookieConsent404"
-                    style={{ visibility: 'hidden' }}
-                    debug={true}
+        <>
+          {btnAcceptState && (
+                <div className={`${hero["containerCookies"]} `} style={{ position: 'absolute', width: '800px'}}>
+                <div className={`${hero["hero_bottom"]} `}>
+                  <div
+                    className={`${hero["hero_bottom__container"]} aspect_ratio aspect_ratio--16by9`}
                   >
-                  </CookieConsent>
-                  <video
-                src="/cookies.mp4"
-                className={hero["hero_video__containerCokies"]}
-                autoPlay
-                ref={ref}
-              >
-                <source src="/cookies.mp4" type="video/mp4"></source>
-              </video>
+                    <div
+                      className={`${hero["hero_bottom__frame"]} ${hero["hero_bottom__frame--one"]}`}
+                    ></div>
+                    <div
+                      className={`${hero["hero_bottom__frame"]} ${hero["hero_bottom__frame--two"]}`}
+                    ></div>
+        
+        
+                    <div
+                      className={`${hero["hero_bottom__frame"]} ${hero["hero_bottom__frame--three"]} aspect_ratio__item object_fit_cover div-video`}
+                    >
+                      <div className={`${hero["hero_container_text2"]}`}>
+                        <div><h1 className={`${hero["hero_text_title"]}`}  style={{ textAlign: 'center', color: 'yellow', fontWeight: '700', fontFamily: 'Helvetica' }}>WELCOME TO OUR WORLD</h1></div>
+                        <div><h2 className={`${hero["hero_text_subtitle"]}`}  style={{ textAlign: 'center',  fontWeight: '700',fontFamily: 'Helvetica'}}>AVISO DE COOKIES</h2></div>
+                      </div>
+                      <span className={hero["hero_container_btn"]} style={{ display: 'flex', justifyContent: ' center' }}>
+                        <button
+                        className={`${hero["hero_btn_ok"]}`}
+                        style={{ border: "none", color: 'blue', fontWeight: '700', marginLeft: "0px", cursor: 'pointer'}}
+                        onClick={aceptFunction}>OK</button>
+                        <button 
+                        style={{ border: "none", color: 'blue', fontWeight: '700', cursor: 'pointer'}}
+                        className={`${hero["hero_btn_cancel"]}`}
+                        onClick={cancelFuncion}
+                        >CANCEL
+                        </button>
+                      </span>
+                      <div style={{ }}>
+          
+                        <video
+                      src="/cookies.mp4"
+                      className={hero["hero_video__containerCokies"]}
+                      autoPlay
+                      ref={ref}
+                    >
+                      <source src="/cookies.mp4" type="video/mp4"></source>
+                    </video>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
-          </div>
+                </div>
+           )}
+        {
+
+      }
+     
+          </>
   );
 }
